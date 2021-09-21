b35423b19726a51b67842b34fe778fd50dd35f80
Kaio Augusto Gasparini <57850048+kaio1077@users.noreply.github.com>
2021-09-21 19:04:55 -0300

    Primeiro Commit
    Código com erro, duplicação de objetos

---
+++ Home.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List _listaTarefas = [];
+  TextEditingController _controllerTarefa = TextEditingController();
+
+  Future <File> _getFile() async {
+    final diretorio = await getApplicationDocumentsDirectory();
+    return File("${diretorio.path}/dados.json");
+  }
+
+  _salvarArquivo() async {
+    var arquivo = await _getFile();
+    // print ("Caminho: " + diretorio.path);
+
+    // Criar dados
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = "Estudar";
+    tarefa["realizada"] = false;
+    _listaTarefas.add(tarefa);
+
+    String dados = json.encode(_listaTarefas); // converter lista de objetos em json
+    arquivo.writeAsString(dados);
+
+  }
+
+  _lerArquivo() async {
+    try{
+      final arquivo = await _getFile();
+      return arquivo.readAsString();
+    }
+    catch(e){
+      return null;
+    }
+  }
+
+  _salvarTarefa(){
+    String textoDigitado = _controllerTarefa.text;
+
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = textoDigitado;
+    tarefa["realizada"] = false;
+
+    setState(() {
+      _listaTarefas.add(tarefa);
+    });
+
+    _salvarArquivo();
+    _controllerTarefa.text = "";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _lerArquivo().then((dados){
+      setState(() {
+        _listaTarefas = json.decode(dados);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    _salvarArquivo();
+    print("itens: " + _listaTarefas.toString());
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de tarefas"),
+        backgroundColor: Colors.purple,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          showDialog(
+              context: context,
+              builder: (context){
+                return AlertDialog(
+                  title: Text("Adicionar Tarefa"),
+                  content: TextField(
+                    controller: _controllerTarefa,
+                    decoration: InputDecoration(
+                      labelText: "Digite sua tarefa"
+                    ),
+                    onChanged: (text){
+
+                    },
+                  ),
+                  actions: [
+                    FlatButton(
+                      child: Text("Cancelar"),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    FlatButton(
+                      child: Text("Salvar"),
+                      onPressed: (){
+                        // salvar
+                        _salvarTarefa();
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                );
+              }
+          );
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.purple,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                itemCount: _listaTarefas.length,
+                  itemBuilder: (context, index){
+
+                  return CheckboxListTile(
+                    title: Text(_listaTarefas[index]["titulo"]),
+                      value: _listaTarefas[index]["realizada"],
+                      onChanged: (valorAlterado){
+                        setState(() {
+                          _listaTarefas[index]['realizada'] = valorAlterado;
+                        });
+                        _salvarArquivo();
+                      }
+                  );
+                  /*
+                    return ListTile(
+                      title: Text(_listaTarefas[index]["titulo"]),
+                    );
+                    Comentário para testar o CheckboxListTile
+                   */
+                  }
+              ),
+          )
+        ],
+      ),
+    );
+  }
+}
